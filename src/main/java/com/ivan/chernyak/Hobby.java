ef4420b49958b8a53e4d49c008f13c8cd0c064ac
ivanChernyak <balonka8521>
2018-10-28 20:52:04 +0200

    Initial commit

---
+++ src/main/java/com/ivan/chernyak/Hobby.java
@@ -0,0 +1,39 @@
+package com.ivan.chernyak;
+
+public class Hobby {
+    private String name;
+    private int hours;
+
+    public Hobby(String name, int hours) {
+        this.name = name;
+        this.hours = hours;
+    }
+
+    public Hobby(String name) {
+        this.name = name;
+    }
+
+    public Hobby(int hours) {
+        this.hours = hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String tellAboutHobby() {
+        return "Name - " + name + "; Hours - " + hours;
+    }
+}
